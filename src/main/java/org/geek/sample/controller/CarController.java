18aff07778367e399e6e6cc6430fd82e5d055ea7
Freddie Johnson Jr <31199155+ig33kmor3@users.noreply.github.com>
2020-08-29 01:20:48 -0700

    added cors

--- src/main/java/org/geek/sample/controller/CarController.java
+++ src/main/java/org/geek/sample/controller/CarController.java
@@ -2,6 +2,7 @@ package org.geek.sample.controller;
 
 import org.geek.sample.model.Car;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
+@CrossOrigin(origins = {"http://localhost:4200", "https://api.curiousquirrel.io"})
 public class CarController {
 
     public CarController() { }
